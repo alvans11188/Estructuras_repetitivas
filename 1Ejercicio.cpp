8860a8033a2bf4ae9fcd585029a71f5f663de5ca
Alexander <achicallag@unjbg.edu.pe>
2024-06-18 23:06:14 -0500

    Se agrego una estructura selectiva para evitar ingresar valores menores a 0

--- 1Ejercicio.cpp
+++ 1Ejercicio.cpp
@@ -9,13 +9,19 @@ int main(){
 	int n, i, m7;
 	cout <<" Ingresa la cantidad de numeros multiplos de 7: ";
 	cin >> n;
-	cout << "MULTIPLICANDO  MULTIPLICADOR  PRODUCTO" << endl;
-	i=1;
-	do{
-		m7 = i * 7;
-		cout << "    "<< i << "         * " << "       "<<7 << "     = "<< m7 << endl;
-		i++;	
-	}while(i<=n);
+
+	if (n>0){
+			cout << "MULTIPLICANDO  MULTIPLICADOR  PRODUCTO" << endl;
+		i=1;
+		do{
+			m7 = i * 7;
+			cout << "    "<< i << "         * " << "       "<<7 << "     = "<< m7 << endl;
+			i++;	
+		}while(i<=n);
+	}else{
+		cout << "Se ingreso un dato invalido"<< endl;
+	}
+
 	
 	return 0;
 }
