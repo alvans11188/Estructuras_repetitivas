bf427faf80f51dba66df70fe9d48f9558c13873d
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-25 13:39:20 -0500

    Jhonel: Mejore interfaz, variables y agregue una condicion de repiticion mas || 1Ejercicio

--- 1Ejercicio.cpp
+++ 1Ejercicio.cpp
@@ -1,27 +1,29 @@
-/*Diseñar un algoritmo que imprima en la pantalla los primeros “n” múltiplos de 7 (positivos 
-mayores que cero)
-*/
-
 #include <iostream>
 using namespace std;
+//Diseñar un algoritmo que imprima en la pantalla los primeros “n” múltiplos de 7 (positivos mayores que cero).
+//ENTRADA|| cantidadnumeros: cantidad de numeros multiplos de 7 a mostrar 		
+//SALIDA|| multiplode7: numeros multiplos de 7
 
 int main(){
-	int n, i, m7;
-	cout <<" Ingresa la cantidad de numeros multiplos de 7: ";
-	cin >> n;
-
-	if (n>0){
-		
-		i=1;
-		do{
-			m7 = i * 7;
-			cout << "Multiplicando  "<< i << " por  "<<7 << " el producto es: "<< m7 << endl;
-			i++;	
-		}while(i<=n);
-	}else{
-		cout << "Se ingreso un dato invalido"<< endl;
-	}
-
-	
+	int cantidadnumeros, i, multiplode7;
+	cout << "**************************************************" << endl << endl;
+	do{
+		cout <<"Ingresa la cantidad de numeros multiplos de 7: ";
+		cin >> cantidadnumeros;
+		cout << endl;
+		if (cantidadnumeros>0){
+			i=1;
+			do{
+				multiplode7 = i * 7;
+				cout << i << ". multiplo: " << multiplode7 << endl;
+				i++;	
+			} while(i<=cantidadnumeros);
+		}else if (cantidadnumeros == 0){
+			cout << "Ok, no se mostrara nada.";
+		} else{
+			cout << "Se ingreso un dato invalido. Intente de nuevo."<< endl;
+		}
+		cout << endl;
+	} while(cantidadnumeros<0);
 	return 0;
 }
